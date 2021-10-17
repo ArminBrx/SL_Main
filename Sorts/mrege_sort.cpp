66e035f1f2b7a50db3c7099b57960345c693c821
Armin L <lohrasbi.armin@gmail.com>
2021-10-17 14:10:16 +0330

    first function

--- Sorts/mrege_sort.cpp
+++ Sorts/mrege_sort.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void meregeSort(int array, int left, int right){
+    //finding our middle index
+    int middle;
+    //in this if we will sort our left and right array
+    if(left < right){
+        //these two are doing recursive function until they are sorted
+        meregeSort(array, left, right);
+        meregeSort(array, middle+1, right);
+        //this is new function for sorting our left and right array
+        //meregeit(array, left, middle, right);
+    }
+}
+
 int main(){
     //getting the size of our array
     int n ;
@@ -12,8 +25,10 @@ int main(){
     for(int i = 0; i < n ; i++){
         cin >> arr[i] ;
     }
-    //show our inputs - test 
+    /* show our inputs - test 
     for(int i = 0 ; i < n ; i++){
         cout << arr[i] << " " ;
-    }
+    } */
+    // sending our array to merege function
+    meregeSort(arr, 0, n-1);
 }
