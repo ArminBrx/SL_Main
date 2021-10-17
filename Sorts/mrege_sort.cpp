1049d739b62b94c3eacf2c61aac1a73eea5df9d6
Armin L <lohrasbi.armin@gmail.com>
2021-10-17 14:35:55 +0330

    second function and fiding lengths

--- Sorts/mrege_sort.cpp
+++ Sorts/mrege_sort.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// now creating new function (mergeit)
+void meregeit(int *array, int left, int middle, int right){
+    int i, j, flag, nleft, nright;
+    //finding our 2 sub arrays length
+    nleft = (middle - left) + 1;
+    nright = right - middle;
+}
+
 void meregeSort(int *array, int left, int right){
     //finding our middle index
     int middle;
@@ -10,7 +18,7 @@ void meregeSort(int *array, int left, int right){
         meregeSort(array, left, right);
         meregeSort(array, middle+1, right);
         //this is new function for sorting our left and right array
-        //meregeit(array, left, middle, right);
+        meregeit(array, left, middle, right);
     }
 }
 
